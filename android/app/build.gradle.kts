a167dcd4bd3d81a270c9e28c391c0c8d6e9ebeea
Rindra <112379212+rindraleon@users.noreply.github.com>
2026-07-18 13:40:46 +0300

    Update compileSdk, minSdk, and targetSdk versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.paroisse_tresorerie"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = compileSdkVersion 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.paroisse_tresorerie"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdkVersion 21
+        targetSdkVersion 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
